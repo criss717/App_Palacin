930dccfcbb5974c002bdc492c753fd1734c5e713
criss717 <mono-717@hotmail.com>
2023-12-29 17:56:31 +0100

    formClient

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,29 @@
 import './App.css'
-import  {Routes,Route} from 'react-router-dom'
+import  {Routes,Route, useLocation} from 'react-router-dom'
 import ShowClients from './views/ShowClients/ShowClients'
 import ShowBatidoras from './views/ShowBatidoras/ShowBatidoras'
 import ShowReductores from './views/ShowReductores/ShowReductores'
+import FormClient from './views/FormClient/FormClient'
 
 
 function App() { 
+  //hooks
+  const location=useLocation()
   
   return (
-    <div>  
+    <div> 
+      {
+        location.pathname!=='/postClient' && (<>
+          <button className='btn btn-primary'>Clientes</button>   
+          <ShowClients/>
+          <ShowBatidoras/>
+          <ShowReductores/>
+        
+        </>)
+      } 
       <Routes>
-        <Route path='/' />
+        <Route path='/postClient' element={<FormClient/>}/>
       </Routes>
-      <button className='btn btn-primary'>Clientes</button>   
-      <ShowClients/>
-      <ShowBatidoras/>
-      <ShowReductores/>
     </div>
   )
 }
--- client/src/views/FormClient/FormClient.jsx
+++ client/src/views/FormClient/FormClient.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 function FormClient() {
     return (
         <div>
-            
+            <div class="form-floating mb-3">
+                <input type="text" className="form-control" id="floatingInput" placeholder="name@example.com" />
+                <label for="floatingInput">Full Name</label>
+            </div>
+            <div className="form-floating">
+                <input type="text" className="form-control" id="floatingPassword" placeholder="Password" />
+                <label for="floatingPassword">Number</label>
+            </div>
+            <div class="col-md">
+                <div class="form-floating">
+                    <select class="form-select" id="floatingSelectGrid">
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                    <label for="floatingSelectGrid">Works with selects</label>
+                </div>
+            </div>
+
         </div>
     )
 }
